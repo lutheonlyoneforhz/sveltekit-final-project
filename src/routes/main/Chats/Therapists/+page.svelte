4c56562f8c6acc2116438be015c924f1579ca25f
gwen13179 <channyeinnyeinlu15@gmail.com>
2025-08-22 15:10:19 +0630

    SvelteKit Final Project

--- src/routes/main/Chats/Therapists/+page.svelte
+++ src/routes/main/Chats/Therapists/+page.svelte
@@ -5,11 +5,10 @@
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
       </div>
       <ul
-        tabindex="0"
         class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="/">Portfolio</a></li>
+        <li><a href="/">About</a></li>
         
       </ul>
     </div>
@@ -19,12 +18,14 @@
   class="toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:var(--color-sky-500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:var(--color-blue-900)]" />
   </div>
   <div class="navbar-center">
-    <a class="btn btn-ghost text-xl">Mind Space</a>
+    <a  href="/" class="btn btn-ghost text-xl">Mind Space</a>
   </div>
   <div class="navbar-end">
+    <!-- svelte-ignore a11y_consider_explicit_label -->
     <button class="btn btn-ghost btn-circle">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
     </button>
+    <!-- svelte-ignore a11y_consider_explicit_label -->
     <button class="btn btn-ghost btn-circle">
       <div class="indicator">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
@@ -49,18 +50,18 @@
 
      
       <ul class="menu p-5 h-110">
-        <li><a class="active"><i class="fa fa-home"></i> Home</a></li>
-        <li><a><i class="fa fa-chart-bar"></i> Dashboard</a></li>
-        <li><a><i class="fa fa-users"></i> Clients</a></li>
-        <li><a><i class="fa fa-calendar"></i> Calendar</a></li>
-        <li><a><i class="fa fa-sticky-note"></i> Notes</a></li>
-        <li><a><i class="fa fa-cog"></i> Settings</a></li>
+        <li><a  href="/" class="active"><i class="fa fa-home"></i> Home</a></li>
+        <li><a href="/" ><i class="fa fa-chart-bar"></i> Dashboard</a></li>
+        <li><a href="/"> <i class="fa fa-users"></i> Clients</a></li>
+        <li><a href="/"><i class="fa fa-calendar"></i> Calendar</a></li>
+        <li><a href="/"><i class="fa fa-sticky-note"></i> Notes</a></li>
+        <li><a href="/"><i class="fa fa-cog"></i> Settings</a></li>
       </ul>
     </div>
 
     
     <div class="p-4 border-bottom border-white/5">
-      <a class="btn btn-sm w-full"><i class="fa fa-sign-out"></i> Log Out</a>
+      <a href="/" class="btn btn-sm w-full"><i class="fa fa-sign-out"></i> Log Out</a>
     </div>
   </aside>
   </div>
