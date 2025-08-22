fbef14d806da77ff759e02f35a3b18b996126cdc
gwen13179 <channyeinnyeinlu15@gmail.com>
2025-08-22 13:38:22 +0630

    SvelteKit Final Project

---
+++ src/routes/main/Chats/Therapists/+page.svelte
@@ -0,0 +1,131 @@
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+        
+      </ul>
+    </div>
+    <input
+  type="checkbox"
+  value="synthwave"
+  class="toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:var(--color-sky-500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:var(--color-blue-900)]" />
+  </div>
+  <div class="navbar-center">
+    <a class="btn btn-ghost text-xl">Mind Space</a>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+    </button>
+    <button class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+        <span class="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+  </div>
+</div>
+<div class="flex h-screen">
+  <div class="w-64 bg-neutral text-neutral-content flex flex-col items-center py-6">
+    <div class="avatar mb-4">
+      <div class="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-1">
+        <img src="https://i.pravatar.cc/100" alt="User Avatar" />
+      </div>
+    </div>
+
+    <h1 class="text-xl font-bold mb-2">La Yung Htut</h1>
+    <h2 class="text-xs mb-5">layunghtut11@gmail.com</h2>
+   <aside class="w-64 bg-white-700 text-white flex flex-col justify-between">
+    <div>
+    
+
+     
+      <ul class="menu p-5 h-110">
+        <li><a class="active"><i class="fa fa-home"></i> Home</a></li>
+        <li><a><i class="fa fa-chart-bar"></i> Dashboard</a></li>
+        <li><a><i class="fa fa-users"></i> Clients</a></li>
+        <li><a><i class="fa fa-calendar"></i> Calendar</a></li>
+        <li><a><i class="fa fa-sticky-note"></i> Notes</a></li>
+        <li><a><i class="fa fa-cog"></i> Settings</a></li>
+      </ul>
+    </div>
+
+    
+    <div class="p-4 border-bottom border-white/5">
+      <a class="btn btn-sm w-full"><i class="fa fa-sign-out"></i> Log Out</a>
+    </div>
+  </aside>
+  </div>
+
+  <div class="flex-1 p-6">
+    <div class="flex justify-between items-center mb-6">
+      <div class="justify-center"></div>
+      
+      
+      <div class="flex items space-x-8">
+
+      <div class="chat chat-start">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+      />
+    </div>
+  </div>
+  <div class="chat-header">
+    Obi-Wan Kenobi
+    <time class="text-xs opacity-50">12:45</time>
+  </div>
+  <div class="chat-bubble">You were the Chosen One!</div>
+  <div class="chat-footer opacity-50">Delivered</div>
+</div>
+<div class="chat chat-end">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+      />
+    </div>
+  </div>
+  <div class="chat-header">
+    Anakin
+    <time class="text-xs opacity-50">12:46</time>
+  </div>
+  <div class="chat-bubble">I hate you!</div>
+  <div class="chat-footer opacity-50">Seen at 12:46</div>
+</div>
+
+        
+        
+       
+      </div>
+      
+    </div>
+
+    
+    </div>
+</div>
+
+
+
+      
+
+        
+        
+       
+      
+      
+    
+
+    
+    
